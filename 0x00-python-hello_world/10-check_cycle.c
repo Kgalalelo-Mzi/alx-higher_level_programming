afc70fde0dd62adc0de598ba6dd4ff8080190002
Kgalalelo-Mzi <kgalalelomzingaye@gmail.com>
2023-06-07 12:12:56 -0700

    Hi

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * last_cycle – checks if there is a cycle within this function 
+ * @list: points to the first node
+ * Return: 0 if success , 1 otherwise
+ */
+int last_cycle(listint_t *list)
+{
+	listint_t *first, *last;
+
+	if (list == NULL || list->next_node == NULL)
+		return (0);
+	first = list;
+	last = first->next_node;
+
+	while (first != NULL && last->next_node != NULL
+		&& last->next_node->next_node != NULL)
+	{
+		if (first == last)
+			return (1);
+		first = first->next_node;
+		last = last->next_node->next_node;
+	}
+	return (0);
+}
