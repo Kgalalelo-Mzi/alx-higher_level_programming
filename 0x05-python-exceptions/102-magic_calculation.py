0f978cb97cd25b5d917807f722a3bc2cad580ed5
Kgalalelo-Mzi <kgalalelomzingaye@gmail.com>
2023-06-26 13:43:22 -0700

    Hi

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    final = 0
+    one = 1
+    three = 3
+    for initial in range(one, three):
+        try:
+            if initial > a:
+                raise Exception('Too far')
+            else:
+                final += a ** b / initial
+        except:
+            final = b + a
+            break
+    return (final)
